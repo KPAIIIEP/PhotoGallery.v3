84098b58bce6cba3a434831734902c7c78bc8292
Crush <art60@rambler.ru>
2020-12-21 13:24:48 +0300

    Add RxJava2, update UI

--- app/src/main/java/ru/study/photogalleryv3/FlickrApi.kt
+++ app/src/main/java/ru/study/photogalleryv3/FlickrApi.kt
@@ -1,19 +1,22 @@
 package ru.study.photogalleryv3
 
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FlickrApi {
     @GET("?method=flickr.photos.getRecent&format=json&nojsoncallback=1&extras=url_s")
-    fun getPhotos(@Query("api_key") api_key: String = API_KEY): Call<FlickrPhotosResponse>
+    fun getPhotos(@Query("api_key") api_key: String = API_KEY): Observable<FlickrPhotosResponse>
 
     companion object {
         private const val BASE_URL = "https://api.flickr.com/services/rest/"
         val flickrApi: FlickrApi = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(FlickrApi::class.java)
--- app/src/main/java/ru/study/photogalleryv3/MainActivity.kt
+++ app/src/main/java/ru/study/photogalleryv3/MainActivity.kt
@@ -2,36 +2,44 @@ package ru.study.photogalleryv3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    lateinit var photoAdapter: PhotoAdapter
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val linearLayoutManager = LinearLayoutManager(this)
-        val photoAdapter = PhotoAdapter(mutableListOf())
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+        photoAdapter = PhotoAdapter(mutableListOf())
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
             layoutManager = linearLayoutManager
             adapter = photoAdapter
         }
 
-        FlickrApi.flickrApi.getPhotos().enqueue(object : Callback<FlickrPhotosResponse> {
-            override fun onResponse(call: Call<FlickrPhotosResponse>, response: Response<FlickrPhotosResponse>) {
-                val result = response.body()
-                result?.let {
-                    photoAdapter.photos = it.response.photos
+        findViewById<Button>(R.id.button).apply {
+            setOnClickListener {
+                update()
+            }
+        }
+
+        update()
+    }
+
+    private fun update() {
+        FlickrApi.flickrApi.getPhotos()
+                .map { result -> result?.let { it.response.photos } }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { photos ->
+                    photoAdapter.photos = photos
                     recyclerView.adapter?.notifyDataSetChanged()
                 }
-            }
-
-            override fun onFailure(call: Call<FlickrPhotosResponse>, t: Throwable) {
-
-            }
-        })
     }
 }
